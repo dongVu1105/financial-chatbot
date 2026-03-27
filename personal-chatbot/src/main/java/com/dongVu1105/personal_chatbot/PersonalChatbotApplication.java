08cce50212ed61a693efd67c5a4bb45f0c853d65
Vu Huy Dong <huydongvu0105@gmail.com>
2026-03-27 14:00:38 +0700

    dongvh: initial backend base

---
+++ personal-chatbot/src/main/java/com/dongVu1105/personal_chatbot/PersonalChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.dongVu1105.personal_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalChatbotApplication.class, args);
+	}
+
+}
---
+++ personal-chatbot/src/test/java/com/dongVu1105/personal_chatbot/PersonalChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongVu1105.personal_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
